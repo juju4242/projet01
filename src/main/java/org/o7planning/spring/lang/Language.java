cc86f686fed3acf22c62bdc3f1c6822a1603eec0
Juju <juju@example.com>
2021-12-25 20:12:28 +0100

    1er commit

---
+++ src/main/java/org/o7planning/spring/lang/Language.java
@@ -0,0 +1,12 @@
+package org.o7planning.spring.lang;
+
+// A Language
+public interface Language {
+
+    // Get a greeting
+    public String getGreeting();
+
+    // Get a bye
+    public String getBye();
+
+}
